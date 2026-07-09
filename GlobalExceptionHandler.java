4217c1213cf36d1d3f4e1309a38b39b0e319d799
Poonam.T <poonamtuptewar@gmail.com>
2026-07-09 16:25:38 +0530

    GlobalExceptionHandler.java

---
+++ GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.example.Student.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(StudentNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleStudentException(StudentNotFoundException ex) {
+
+        ErrorResponse error =
+                new ErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND.value());
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+}
